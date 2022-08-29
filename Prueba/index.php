2aebb4489e1afd89529e9edb4c05532a967c2ab4
Santiago Gonzalez <90287212+SantiagoG24@users.noreply.github.com>
2022-08-29 17:05:14 -0300

    agrege cambios
    dividi footer y header

--- Prueba/index.php
+++ Prueba/index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <title>Prueba</title>
-</head>
-<body>
+
 <?php 
+require_once 'header.html';
 require_once 'noticia.php';
 ?>
 <nav class="navbar navbar-dark bg-dark">
@@ -40,22 +32,16 @@ require_once 'noticia.php';
 <?php
 foreach($noticias as $noticia){
 ?>
-<div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title"><?php echo $noticia->title?></h5>
-        <p class="card-text"><?php echo $noticia->text?></p>
-      </div>
-    </div>
+<div class="card" style="width: 18rem;">
+  <img src="<?php echo $noticia->img?>" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $noticia->title?></h5>
+    <p class="card-text"><?php echo $noticia->text?></p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
 <?php
 }
-?>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-</body>
-</html>
+require_once 'footer.html';
+
+  
--- Prueba/noticia.php
+++ Prueba/noticia.php
@@ -1,17 +1,26 @@
 <?php
 $n0 =new stdClass();
-$n0 ->title='La terrible mufa del gomez ';
-$n0 ->text='El gomez es la persona del discord que no consiguo a Messi en un juego de mierda del albun del fifa y se lo bardea sin razon.';
+$n0 ->title='¿Por qué Leclerc y Verstappen partirán desde el fondo en Bélgica?';
+$n0 ->text='Las vacaciones de la Fórmula 1 llegaron a su fin. Ya pasó el descanso de cada verano y, en consecuencia, los pilotos se encuentran listos para encarar el tramo final de la temporada. Y el regreso a la actividad se está dando en el Gran Premio de Bélgica, que comenzó con dos prácticas libres bajo la lluvia en las que el dominio de las tandas se dividió entre Max Verstappen, el más rápido en la segunda sesión (le sacó casi un segundo al resto), y los dos hombres de Ferrari, Charles Leclerc y Carlos Sainz.';
+$n0->img='img/f1_noticia.png';
+
 $n1 =new stdClass();
-$n1 ->title='La terrible mufa del gomez ';
-$n1 ->text='El gomez es la persona del discord que no consiguo a Messi en un juego de mierda del albun del fifa y se lo bardea sin razon.';
+$n1 ->title='Lorem 2 ';
+$n1 ->text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cumque perferendis esse quia veniam commodi eius quibusdam quasi officia architecto maiores asperiores nobis quidem praesentium iure, sed eligendi vel aliquid.';
+$n1->img='img/f1_noticia.png';
+
 $n2 =new stdClass();
-$n2 ->title='La terrible mufa del gomez ';
-$n2 ->text='El gomez es la persona del discord que no consiguo a Messi en un juego de mierda del albun del fifa y se lo bardea sin razon.';
+$n2 ->title='Lorem 3';
+$n2 ->text='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae cumque perferendis esse quia veniam commodi eius quibusdam quasi officia architecto maiores asperiores nobis quidem praesentium iure, sed eligendi vel aliquid.';
+$n2->img='/img/f1_noticia.png';
 
 
 $noticias= array(
     "noticia0"=>$n0,
     "noticia1"=>$n1,
-    "notixia2"=>$n2
- );
+    "noticia2"=>$n2
+);
+?>
+<!-- <h1><?php echo $noticia->title?></h1>
+<img src="" alt="">
+<p><?php echo $noticia->text?></p> -->
